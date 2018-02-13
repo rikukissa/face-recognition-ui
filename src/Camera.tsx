a2d093b699c82c6b079339f0d0ceefef5d87f7ed
Riku Rouvila <riku.rouvila@gmail.com>
2018-02-13 22:24:59 +0000

    stretch camera full screen

--- src/Camera.tsx
+++ src/Camera.tsx
@@ -6,9 +6,24 @@ import { getStream } from "./lib/camera";
 
 declare const tracking: any;
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
 
-const Video = styled.video``;
+const Video = styled.video`
+  position: absolute;
+  top: -240px;
+  left: -320px;
+`;
+const Canvas = styled.canvas`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+`;
 
 export interface IFaceRect {
   width: number;
@@ -35,61 +50,95 @@ function createFaceImage($video: HTMLVideoElement): Promise<Blob> {
   );
 }
 
+interface IDetectionEvent {
+  data: IFaceRect[];
+}
+
+function createThrottler() {
+  let firstEmptyTime: null | number = 0;
+  return function shouldEmit(event: IDetectionEvent) {
+    const noFaces = event.data.length === 0;
+
+    if (firstEmptyTime === null && noFaces) {
+      firstEmptyTime = Date.now();
+    }
+
+    if (noFaces) {
+      const longEnoughWithoutNoFaces =
+        noFaces && Date.now() - (firstEmptyTime as number) > 3000;
+      if (!longEnoughWithoutNoFaces) {
+        return false;
+      }
+    } else {
+      firstEmptyTime = null;
+    }
+    return true;
+  };
+}
+
 export class Camera extends React.Component<
   { onFacesDetected: (data: IDetection) => void },
-  { videoWidth: number; videoHeight: number }
+  {}
 > {
-  public state = {
-    videoWidth: 320,
-    videoHeight: 240
-  };
   private $video: HTMLVideoElement;
+  private $canvas: HTMLCanvasElement;
 
   public async componentDidMount() {
     const stream = await getStream();
-    const $video = this.$video;
 
-    $video.setAttribute("width", this.state.videoWidth.toString());
-    $video.setAttribute("height", this.state.videoHeight.toString());
-    $video.setAttribute("autoplay", "true");
-    $video.src = window.URL.createObjectURL(stream);
+    this.$video.setAttribute("width", "320");
+    this.$video.setAttribute("height", "240");
+    this.$video.setAttribute("autoplay", "true");
+    this.$video.src = window.URL.createObjectURL(stream);
+
+    this.$canvas.setAttribute("width", "320");
+    this.$canvas.setAttribute("height", "240");
 
     const tracker = new tracking.ObjectTracker("face") as any;
-    tracker.setInitialScale(4);
+
+    // Smaller is more precise but slower
     tracker.setStepSize(0.5);
+
+    tracker.setInitialScale(4);
     tracker.setEdgesDensity(0.1);
-    tracking.track($video, tracker, { camera: true });
+    tracking.track(this.$video, tracker, { camera: true });
 
-    let firstEmptyTime: null | number = 0;
+    const shouldEmit = createThrottler();
 
-    tracker.on("track", async (event: { data: IFaceRect[] }) => {
+    tracker.on("track", async (event: IDetectionEvent) => {
       /*
        * Tracking sometimes gives random empty results
        * even when there are faces in the picture
        */
-
-      const noFaces = event.data.length === 0;
-
-      if (firstEmptyTime === null && noFaces) {
-        firstEmptyTime = Date.now();
-      }
-
-      if (noFaces) {
-        const longEnoughWithoutNoFaces =
-          noFaces && Date.now() - (firstEmptyTime as number) > 3000;
-        if (!longEnoughWithoutNoFaces) {
-          return;
-        }
-      } else {
-        firstEmptyTime = null;
+      if (!shouldEmit(event)) {
+        return;
       }
 
       this.props.onFacesDetected({
         amount: event.data.length,
-        image: await createFaceImage($video)
+        image: await createFaceImage(this.$video)
       });
     });
+
+    this.reflectVideoToCanvas();
   }
+
+  /*
+   * The reason I'm not just stretching the video is, that it would make
+   * the face detection much slower
+   * More pixels = more surfice to look for the face
+   */
+  private reflectVideoToCanvas = () => {
+    const context = this.$canvas.getContext("2d") as CanvasRenderingContext2D;
+    context.drawImage(
+      this.$video,
+      0,
+      0,
+      this.$canvas.width,
+      this.$canvas.height
+    );
+    window.requestAnimationFrame(this.reflectVideoToCanvas);
+  };
 
   public render() {
     return (
@@ -97,6 +146,11 @@ export class Camera extends React.Component<
         <Video
           innerRef={($el: HTMLVideoElement) => {
             this.$video = $el;
+          }}
+        />
+        <Canvas
+          innerRef={($el: HTMLCanvasElement) => {
+            this.$canvas = $el;
           }}
         />
       </Container>
