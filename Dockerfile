9bff4b3d2d4641c5c1cccc7318785a8ddc82164a
Riku Rouvila <riku.rouvila@gmail.com>
2018-03-20 21:27:04 +0000

    fix deployment config

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM nginx
 COPY build /usr/share/nginx/html
 COPY nginx.template /etc/nginx/conf.d/nginx.template
 
-EXPOSE 8080
+EXPOSE 8000
 
-CMD /bin/bash -c "envsubst '${API_URL}' < /etc/nginx/conf.d/nginx.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'"
+CMD envsubst '${API_URL}' < /etc/nginx/conf.d/nginx.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,9 +4,7 @@ export const PRODUCTION = process.env.NODE_ENV === "production";
 export const API_ROOT = PRODUCTION ? "/api" : "//localhost:3005";
 
 const WS_HOST = PRODUCTION ? document.location.host : "localhost:3005";
-export const WEBSOCKET_ADDRESS = PRODUCTION
-  ? `wss://${WS_HOST}/api`
-  : `ws://${WS_HOST}/api`;
+export const WEBSOCKET_ADDRESS = `ws://${WS_HOST}/api`;
 
 export const DASHBOARD_TIMEOUT = 20000;
 export const WHO_IS_THIS_TIMEOUT = 20000;
